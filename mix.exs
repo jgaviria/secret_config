dd09fcd3014701b714b11ed5a5cea33e92ff8191
Juan Gaviria <juan.gaviria@experian.com>
2020-08-20 23:27:51 -0400

    Fixing mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SecretConfig.MixProject do
   def project do
     [
       app: :secret_config,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
